7bbeb28f7e0a84db0751c9f0e5024c56905f78dc
Andrei Bychko <abychko@aras.com>
2020-12-02 17:42:13 +0300

    Move Relationship class out of MainWindow.

--- Code/IDME.WpfEditor/MainWindow.xaml.cs
+++ Code/IDME.WpfEditor/MainWindow.xaml.cs
@@ -184,38 +184,6 @@ namespace IDME.WpfEditor
 			return connector;
 		}
 
-		private class Relationship
-		{
-			public ItemControl Source { get; }
-
-			public ItemControl Related { get; }
-
-			public Polyline Connector { get; }
-
-			public Relationship(ItemControl source, ItemControl related, Polyline connector)
-			{
-				Source = source;
-				Related = related;
-				Connector = connector;
-			}
-
-			public void UpdateConnector()
-			{
-				Source.UpdateLayout();
-				Related.UpdateLayout();
-
-				double leftX = Source.Item.Left + Source.ActualWidth / 2;
-				double rightX = Related.Item.Left;
-				double topY = Source.Item.Top + Source.ActualHeight;
-				double bottomY = Related.Item.Top + Related.ActualHeight / 2;
-
-				Connector.Points.Clear();
-				Connector.Points.Add(new Point(leftX, topY));
-				Connector.Points.Add(new Point(leftX, bottomY));
-				Connector.Points.Add(new Point(rightX, bottomY));
-			}
-		}
-
 		private readonly IDictionary<Item, List<Relationship>> _incomingRelationships = new Dictionary<Item, List<Relationship>>();
 		private readonly IDictionary<Item, List<Relationship>> _outgoingRelationships = new Dictionary<Item, List<Relationship>>();
 		private readonly IDictionary<Item, ItemControl> _allItemControls = new Dictionary<Item, ItemControl>();
---
+++ Code/IDME.WpfEditor/ViewModels/Relationship.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Shapes;
+
+using IDME.WpfEditor.Controls;
+
+namespace IDME.WpfEditor.ViewModels
+{
+	public class Relationship
+	{
+		public ItemControl Source { get; }
+
+		public ItemControl Related { get; }
+
+		public Polyline Connector { get; }
+
+		public Relationship(ItemControl source, ItemControl related, Polyline connector)
+		{
+			Source = source;
+			Related = related;
+			Connector = connector;
+		}
+
+		public void UpdateConnector()
+		{
+			Source.UpdateLayout();
+			Related.UpdateLayout();
+
+			double leftX = Source.Item.Left + Source.ActualWidth / 2;
+			double rightX = Related.Item.Left;
+			double topY = Source.Item.Top + Source.ActualHeight;
+			double bottomY = Related.Item.Top + Related.ActualHeight / 2;
+
+			Connector.Points.Clear();
+			Connector.Points.Add(new Point(leftX, topY));
+			Connector.Points.Add(new Point(leftX, bottomY));
+			Connector.Points.Add(new Point(rightX, bottomY));
+		}
+	}
+}
